cfba3ce4b93e450b00dbc8f19540c112b1aa894b
ted3x <tedo.manvelidze@space.ge>
2021-02-20 22:14:32 +0400

    removed databinding

--- app/src/main/java/com/example/picnat/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/picnat/ui/login/LoginActivity.kt
@@ -1,17 +1,14 @@
 package com.example.picnat.ui.login
 
 import android.os.Bundle
-import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.PicnatApplication
 import com.example.picnat.R
-import com.example.picnat.databinding.ActivityLoginBinding
 import com.example.picnat.repository.AuthInterface
 import com.example.picnat.ui.auth.AuthViewModel
 import com.example.picnat.ui.auth.AuthViewModelFactory
@@ -30,12 +27,9 @@ class LoginActivity : AppCompatActivity(), AuthInterface {
 
         (application as PicnatApplication).appComponent.inject(this)
 
-        val binding: ActivityLoginBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_login)
+        setContentView(R.layout.activity_login)
 
         viewModel = ViewModelProviders.of(this, viewModelFactory)[AuthViewModel::class.java]
-
-        binding.viewmodel = viewModel
 
         viewModel.authListener = this
 
--- app/src/main/java/com/example/picnat/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/picnat/ui/register/RegisterActivity.kt
@@ -5,15 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.PicnatApplication
+import com.example.picnat.MainActivity
 import com.example.picnat.R
-import com.example.picnat.databinding.ActivityRegisterBinding
 import com.example.picnat.repository.AuthInterface
 import com.example.picnat.ui.auth.AuthViewModel
 import com.example.picnat.ui.auth.AuthViewModelFactory
-import com.example.picnat.MainActivity
 import com.example.picnat.utils.startHomeActivity
 import kotlinx.android.synthetic.main.activity_register.*
 import javax.inject.Inject
@@ -31,10 +29,8 @@ class RegisterActivity : AppCompatActivity(), AuthInterface {
         (application as PicnatApplication).appComponent.registrationComponent().create()
             .inject(this)
 
-        val binding: ActivityRegisterBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_register)
+        setContentView(R.layout.activity_register)
         viewModel = ViewModelProviders.of(this, viewModelFactory)[AuthViewModel::class.java]
-        binding.viewmodel = viewModel
 
         viewModel.authListener = this
 
