0a812569b4ec379f15733520b23b71dab277bd27
ted3x <tedo.manvelidze@space.ge>
2021-02-24 22:09:37 +0400

    edit

--- feature-splash/src/main/java/com/picnat/feature_splash/ui/SplashViewModel.kt
+++ feature-splash/src/main/java/com/picnat/feature_splash/ui/SplashViewModel.kt
@@ -5,24 +5,27 @@ import com.google.firebase.auth.FirebaseAuth
 import com.picnat.core.base.BaseFeatureVM
 import com.picnat.core.data.repository.UserRepositoryImpl
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class SplashViewModel(private val userRepository: UserRepositoryImpl, private val firebaseAuth: FirebaseAuth) : BaseFeatureVM() {
+class SplashViewModel(
+    private val userRepository: UserRepositoryImpl,
+    private val firebaseAuth: FirebaseAuth
+) : BaseFeatureVM() {
 
     init {
         logIn()
     }
 
-    fun logIn(){
-        if(firebaseAuth.currentUser != null){
-            viewModelScope.launch(Dispatchers.IO) {
+    fun logIn() {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (firebaseAuth.currentUser != null)
                 firebaseAuth.currentUser?.uid?.let { userRepository.getUser(it) }
-            }
-        }
-        else
-            viewModelScope.launch {
+            else {
+                delay(1000)
                 globalNavigator.loadAuthFeature()
             }
+        }
     }
 
 }
