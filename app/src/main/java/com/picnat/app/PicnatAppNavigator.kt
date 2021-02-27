afccb59006338b508db55ce19d0c6719a3815df1
bekka <beqachachua@gmail.com>
2021-02-27 13:01:46 +0400

    remove annotation

--- app/src/main/java/com/picnat/app/PicnatAppNavigator.kt
+++ app/src/main/java/com/picnat/app/PicnatAppNavigator.kt
@@ -1,9 +1,7 @@
 package com.picnat.app
 
 import com.github.terrakok.cicerone.androidx.AppNavigator
-import org.koin.core.component.KoinApiExtension
 
-@KoinApiExtension
 class PicnatAppNavigator(activity: MainActivity, containerRes: Int) :
     AppNavigator(
         activity,
